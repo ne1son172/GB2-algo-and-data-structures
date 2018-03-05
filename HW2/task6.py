75038ee3bfac5b840c13ca998cb89b8459191c99
ne1son172 <ne1son@list.ru>
2018-03-05 14:20:24 +0300

    task 6 is updated

--- HW2/task6.py
+++ HW2/task6.py
@@ -4,3 +4,22 @@
 После каждой неудачной попытки должно сообщаться больше или меньше введенное пользователем число, чем то, что загадано.
 Если за 10 попыток число не отгадано, то вывести загаданное число.
 """
+
+from random import randint
+
+count = 0
+num = randint(0, 100)
+
+while count < 10:
+    guess = int(input("Make a guess >>> "))
+    count += 1
+
+    if guess == num:
+        print('You win!!!')
+        break
+    elif guess < num:
+        print('Guess < Num')
+    else:
+        print('Guess > Num')
+
+print('You lose. Num is: ', num)
